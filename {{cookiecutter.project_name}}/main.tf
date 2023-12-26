cc3884f8d5d772cd3379454c897c5a9552081e51
Joe Hong <jhng323@gmail.com>
2023-12-26 10:44:24 -0500

    initial commit

---
+++ {{cookiecutter.project_name}}/main.tf
@@ -0,0 +1,66 @@
+###############################
+# VERSIONS
+###############################
+terraform {
+  required_version = "~> {{ cookiecutter.terraform_version }}"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= {{ cookiecutter.aws_provider_version }}"
+    }
+  }
+}
+
+###############################
+# PROVIDERS
+###############################
+# https://registry.terraform.io/providers/hashicorp/aws/latest
+provider "aws" {
+  region  = local.region
+  profile = local.profile
+
+  default_tags {
+    tags = {
+      Project = local.project_name
+    }
+  }
+}
+
+###############################
+# LOCALS
+###############################
+locals {
+  region = "{{ cookiecutter.aws_region }}"
+  profile = "{{ cookiecutter.project_profile }}"
+  project_name = "{{ cookiecutter.project_name }}"
+}
+
+###############################
+# DATA SOURCES
+###############################
+# https://developer.hashicorp.com/terraform/language/data-sources
+data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "available" {}
+data "aws_organizations_organization" "this" {}
+
+
+###############################
+# MODULES
+###############################
+
+
+
+###############################
+# RESOURCES
+###############################
+
+
+
+###############################
+# OUTPUTS
+###############################
+
+output "example" {
+  value = "example"
+}
