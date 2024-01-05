b34a692c12a79e7c7635c1f2fd27c597219a7655
Joe Hong <jhng323@gmail.com>
2024-01-05 14:11:28 -0500

    update outputs.tf with data source outputs and add .gitignore file

--- {{cookiecutter.project_name}}/main.tf
+++ {{cookiecutter.project_name}}/main.tf
@@ -61,6 +61,14 @@ data "aws_organizations_organization" "this" {}
 # OUTPUTS
 ###############################
 
-output "example" {
-  value = "example"
+output "account-id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "azs" {
+  value = data.aws_availability_zones.available.names
+}
+
+output "aws-org-id" {
+  value = data.aws_organizations_organization.this.id
 }
